fd5de28c02f10f9ea47a8f5f5bf81f0b106dc66d
AnoopVL <avlanjekar4@gmail.com>
2025-02-26 01:04:16 +0530

    ws-server endpoints initialized

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,8 +1,20 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", function connection(ws, request) {
+  const url = request.url;
+  if (!url) {
+    request;
+  }
+  const queryParams = new URLSearchParams(url?.split("?")[1]);
+  const token = queryParams.get("token") || "";
+  const decoded = jwt.verify(token, JWT_SECRET);
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
   ws.on("message", function connection(data) {
     ws.send("pong");
   });
