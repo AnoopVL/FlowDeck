7c7d021031cf8f8fccb0ca7a8a3fdae413f73969
AnoopVL <avlanjekar4@gmail.com>
2025-02-26 00:50:16 +0530

    http endpoints, middleware initialize

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,4 +1,25 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
+
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  res.json({
+    userId: "123",
+  });
+});
+
+app.post("/signin", async (req, res) => {
+  const userId = 123;
+  const token = jwt.sign({ userId }, JWT_SECRET);
+  res.json(token);
+});
+
+app.post("/room", async (req, res) => {});
 
 app.listen(3001);
