02481e13d447cec5f90303de9b4490ddb07c3152
AnoopVL <avlanjekar4@gmail.com>
2025-02-26 22:26:25 +0530

    db packages updated

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "./config";
-import { CreateUserSchema } from "@repo/common/types";
+import {
+  CreateRoomSchema,
+  CreateUserSchema,
+  SigninSchema,
+} from "@repo/common/types";
 // import dotenv from "dotenv";
 // dotenv.config();
 
@@ -11,17 +15,41 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
   res.json({
     userId: "123",
   });
 });
 
 app.post("/signin", async (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
   const userId = 123;
   const token = jwt.sign({ userId }, JWT_SECRET);
   res.json(token);
 });
 
-app.post("/room", async (req, res) => {});
+app.post("/room", async (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+  res.json({
+    roomId: 21,
+  });
+});
 
 app.listen(3001);
