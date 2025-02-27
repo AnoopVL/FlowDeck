6bd0dbc43607ca304bc7c990dde7c200753cdad0
AnoopVL <avlanjekar4@gmail.com>
2025-02-28 01:56:26 +0530

    get messages endpoints

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -103,4 +103,21 @@ app.post("/room", middleware, async (req, res) => {
     });
   }
 });
+// add middleware to the below endpoint
+app.get("/chat/:roomId", async (req, res) => {
+  const roomId = Number(req.params.roomId);
+  const messages = await prismaClient.chat.findMany({
+    where: {
+      roomId: roomId,
+    },
+    orderBy: {
+      id: "desc",
+    },
+    take: 50,
+  });
+
+  res.json({
+    messages,
+  });
+});
 app.listen(3001);
