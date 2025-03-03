63d04cd27d9b77e57041f8041692602ed2f53105
AnoopVL <avlanjekar4@gmail.com>
2025-03-04 00:57:51 +0530

    file structure updated

--- apps/flowdeck-fe/app/canvas/[roomId]/page.tsx
+++ apps/flowdeck-fe/app/canvas/[roomId]/page.tsx
@@ -1,49 +1,22 @@
 "use client";
+import { initDraw } from "@/draw";
 import { useEffect, useRef } from "react";
 
 export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas?.getContext("2d");
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
 
-    if (!ctx) {
-      return;
+      initDraw(canvas);
     }
-
-    let startX = 0;
-    let startY = 0;
-    let clicked = false;
-
-    canvas?.addEventListener("mousedown", (e) => {
-      clicked = true;
-      console.log(e.clientX);
-      console.log(e.clientY);
-      startX = e.clientX;
-      startY = e.clientY;
-    });
-    canvas?.addEventListener("mouseup", (e) => {
-      clicked = false;
-      console.log(e.clientX);
-      console.log(e.clientY);
-    });
-
-    canvas?.addEventListener("mousemove", (e) => {
-      if (clicked) {
-        const width = e.clientX - startX;
-        const height = e.clientY - startY;
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.strokeRect(startX, startY, width, height);
-      }
-    });
   }, [canvasRef]);
 
   return (
     <>
       <div>
-        <canvas ref={canvasRef} width={500} height={500}></canvas>
+        <canvas ref={canvasRef} width={1080} height={1000}></canvas>
       </div>
     </>
   );
